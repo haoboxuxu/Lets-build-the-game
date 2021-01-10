f5ac3d26605c4781c2d04146f6ea1f8781a3a8ef
haoboxuxu <haoboxuxu@icloud.com>
2021-01-10 18:13:40 +0800

    SurvivalHorrorGame 8% finished

---
+++ SurvivalHorrorGame/Assets/Scripts/FlameAnimations.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlameAnimations : MonoBehaviour
+{
+
+    public int lightNode;
+    public GameObject flameLight;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (lightNode == 0)
+        {
+            StartCoroutine(animationLight());
+        }
+    }
+
+    IEnumerator animationLight()
+    {
+        lightNode = Random.Range(1, 4);
+        if (lightNode == 1)
+        {
+            flameLight.GetComponent<Animation>().Play("TorchAnim1");
+        }
+        if (lightNode == 2)
+        {
+            flameLight.GetComponent<Animation>().Play("TorchAnim2");
+        }
+        if (lightNode == 3)
+        {
+            flameLight.GetComponent<Animation>().Play("TorchAnim3");
+        }
+
+        yield return new WaitForSeconds(0.99f);
+        lightNode = 0;
+    }
+}
---
+++ SurvivalHorrorGame/Assets/Scripts/Sequences/AOpening.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class AOpening : MonoBehaviour
+{
+
+    public GameObject ThePlayer;
+    public GameObject FadeScreenIn;
+    public GameObject TextBox;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ThePlayer.GetComponent<FirstPersonController>().enabled = false;
+        StartCoroutine(ScenePlayer());
+    }
+
+    IEnumerator ScenePlayer()
+    {
+        yield return new WaitForSeconds(1.5f);
+        FadeScreenIn.SetActive(false);
+        TextBox.GetComponent<Text>().text = "I need to get out";
+        yield return new WaitForSeconds(2);
+        TextBox.GetComponent<Text>().text = "";
+        ThePlayer.GetComponent<FirstPersonController>().enabled = true;
+    }
+}

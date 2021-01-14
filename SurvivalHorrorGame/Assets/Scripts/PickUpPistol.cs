6cdc80cec97834b0b7a34e2322f5b266d024d951
haoboxuxu <haoboxuxu@icloud.com>
2021-01-15 03:51:17 +0800

    SurvivalHorrorGame 15% finished

---
+++ SurvivalHorrorGame/Assets/Scripts/PickUpPistol.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PickUpPistol : MonoBehaviour
+{
+    public float TheDistance;
+    public GameObject ActionDisplay;
+    public GameObject ActionText;
+    public GameObject FakePistol;
+    public GameObject RealPistol;
+    public GameObject GuidArrow;
+    public GameObject ExtraCross;
+
+    // Update is called once per frame
+    void Update()
+    {
+        TheDistance = PlayerCasting.DistanceFromTarget;
+
+    }
+
+    void OnMouseOver()
+    {
+        if (TheDistance <= 2)
+        {
+            ExtraCross.SetActive(true);
+            ActionText.GetComponent<Text>().text = "Pick up Pistol";
+            ActionDisplay.SetActive(true);
+            ActionText.SetActive(true);
+        }
+
+        if (Input.GetButtonDown("Action"))
+        {
+            if (TheDistance <= 2)
+            {
+                this.GetComponent<BoxCollider>().enabled = false;
+                ActionDisplay.SetActive(false);
+                ActionText.SetActive(false);
+                FakePistol.SetActive(false);
+                RealPistol.SetActive(true);
+                ExtraCross.SetActive(false);
+                GuidArrow.SetActive(false);
+            }
+        }
+    }
+
+    void OnMouseExit()
+    {
+        ExtraCross.SetActive(false);
+        ActionDisplay.SetActive(false);
+        ActionText.SetActive(false);
+    }
+}
---
+++ SurvivalHorrorGame/Assets/Scripts/Sequences/BFirstTrigger.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class BFirstTrigger : MonoBehaviour
+{
+    public GameObject ThePlayer;
+    public GameObject TextBox;
+    public GameObject TheMarker;
+
+    void OnTriggerEnter()
+    {
+        ThePlayer.GetComponent<FirstPersonController>().enabled = false;
+        StartCoroutine(ScenePlayer());
+    }
+
+    IEnumerator ScenePlayer()
+    {
+        TextBox.GetComponent<Text>().text = "A weapin on the table";
+        yield return new WaitForSeconds(2f);
+        TextBox.GetComponent<Text>().text = "";
+        ThePlayer.GetComponent<FirstPersonController>().enabled = true;
+        TheMarker.SetActive(true);
+    }
+}
